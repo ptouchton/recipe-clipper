17b9520f6b951349cc5ff0ba44ddb37497a0b7a2
Paul Touchton <p_touchton@yahoo.com>
2020-07-13 18:55:11 -0700

    wired in auth0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Subject, EMPTY } from 'rxjs';
+import { Router, NavigationStart, NavigationEnd, NavigationCancel, NavigationError } from '@angular/router';
+import { AuthService } from './services/auth.service';
+import { catchError } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'recipe-clipper';
+  pageTitle = 'recipe-clipper';
+  loading = true;
+
+  private errorMessageSubject = new Subject<string>();
+  errorMessage$ = this.errorMessageSubject.asObservable();
+
+  get isLoggedIn(): boolean {
+    return this.authService.loggedIn;
+  }
+
+
+  constructor(private authService: AuthService,
+              private router: Router) {
+
+  }
+
+  userName$ = this.authService.userProfile$.pipe(
+    catchError(err => {
+      this.errorMessageSubject.next(err);
+      return EMPTY;
+    })
+  );
+
+
+  logIn(): void {
+    this.authService.login();
+  }
+
+  logOut(): void {
+    this.authService.logout();
+    console.log('Log out');
+  }
 }
