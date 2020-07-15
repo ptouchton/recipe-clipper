00561a7a12e38f0117fb62538ca6206de59057d4
Paul Touchton <p_touchton@yahoo.com>
2020-07-14 20:14:55 -0700

    finished wiring in fauna function call

---
+++ functions/products.js
@@ -0,0 +1,30 @@
+import { ProductService } from '../lib/product.service.js'
+import { client, headers } from '../lib/config.js'
+
+const service = new ProductService({ client })
+
+exports.handler = async (event, context) => {
+	console.log('Function `products` invoked')
+
+	if (event.httpMethod !== 'GET') {
+		return { statusCode: 405, headers, body: 'Method Not Allowed' }
+	}
+
+	try {
+		console.log(`here: ${client.context}`);
+		const products = await service.getProducts()
+		return {
+			statusCode: 200,
+			headers,
+			body: JSON.stringify(products),
+		}
+	} catch (error) {
+		console.log('error', error)
+
+		return {
+			statusCode: 400,
+			headers,
+			body: JSON.stringify(error),
+		}
+	}
+}
