8d7633fff2f4959d3ed47a0177851bb7b368a251
Paul Touchton <p_touchton@yahoo.com>
2020-07-22 21:39:40 -0700

    cleaning up some error handling

--- functions/products.js
+++ functions/products.js
@@ -5,6 +5,7 @@ import jwks from 'jwks-rsa';
 
 const service = new ProductService({ client })
 
+
 exports.handler = async (event, context) => {
 	console.log('Function `products` invoked')
 	console.log(context);
@@ -24,82 +25,73 @@ exports.handler = async (event, context) => {
 	// Set up Auth0 configuration
 	const authConfig = {
 		domain: 'dev-1ri5oc4g.us.auth0.com',
-		audience: 'http://localhost:9000/.netlify/products',
+		audience: 'http://localhost:9000/.netlify/function',
 	};
 
 
-	// const jwtCheck = jwt({
-	// 	secret: jwks.expressJwtSecret({
-	// 		cache: true,
-	// 		rateLimit: true,
-	// 		jwksRequestsPerMinute: 5,
-	// 		jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
-	// 	}),
-	// 	audience: authConfig.audience,
-	// 	issuer: `https://${authConfig.domain}/`,
-	// 	algorithms: ['RS256'],
-	// });
+	const client = jwks({
+		strictSsl: true, // Default value
+		jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+		requestHeaders: {}, // Optional
+		requestAgentOptions: {}, // Optional
+		timeout: 3000, // Defaults to 30s
+	});
 
-
-
-	try {
-
-		// const client = jwks({
-		// 	jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
-		// });
-
-		const client = jwks({
-			strictSsl: true, // Default value
-			jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
-			requestHeaders: {}, // Optional
-			requestAgentOptions: {}, // Optional
-			timeout: 3000, // Defaults to 30s
+	function getKey(header, callback) {
+		client.getSigningKey(header.kid, function (err, key) {
+			let signingKey = key.publicKey || key.rsaPublicKey;
+			callback(null, signingKey);
 		});
-
-		const authHeader = event.headers.authorization;
-		console.log(`jwt: ${event.headers.authorization}`);
-
-		const parts = authHeader.split(" ");
-		if (parts.length !== 2 || parts[0] !== "Bearer") {
-			// No access token was passed
-			return null;
-		}
-		const accessToken = parts[1];
-
-		console.log(`token: ${accessToken}`);
-
-		 // This could fail.  If it does handle as 401 as the token is invalid.
-		 var decodedToken = jwt.decode(accessToken, {complete: true});
-		 console.log(`decoded token: ${decodedToken.header.kid}`);
-		 
-		const kid = decodedToken.header.kid;
-		let signingKey = null;
-
-		client.getSigningKey(kid, (err, key) => {
-			signingKey = key.getPublicKey();
-
-			console.log(`Key: ${signingKey}`);
-
-			const decoded = jwt.verify(accessToken, signingKey,{ audience: authConfig.audience });
-			console.log(`verified: ${decoded}`);
-
-			
-		});
-
-		const products = await service.getProducts()
-			return {
-				statusCode: 200,
-				headers,
-				body: JSON.stringify(products),
-			}
-
-	} catch (error) {
-		console.log('error', error)
-
-		return {
-			statusCode: 400,
-			headers,
-			body: JSON.stringify(error),
-		}
 	}
+
+	const authHeader = event.headers.authorization;
+	console.log(`jwt: ${event.headers.authorization}`);
+
+	const parts = authHeader.split(" ");
+	if (parts.length !== 2 || parts[0] !== "Bearer") {
+		// No access token was passed
+		return null;
+	}
+	const accessToken = parts[1];
+
+	console.log(`token: ${accessToken}`);
+
+	// This could fail.  If it does handle as 401 as the token is invalid.
+	var decodedToken = jwt.decode(accessToken, { complete: true });
+	console.log(`decoded token: ${decodedToken.header.kid}`);
+
+	const header = decodedToken.header;
+	const decoded = jwt.verify(accessToken, getKey, { audience: authConfig.audience }, function (err, decoded) {
+
+		if (err) {
+			console.log(`err: ${err}`);
+			
+			// return {
+			// 	statusCode: 401,
+			// 	headers,
+			// 	body: 'Unauthorized',
+			// }
+		}
+		console.log(`verified: ${decoded}`);
+	});
+
+
+	// const products = await service.getProducts()
+	// return {
+	// 	statusCode: 200,
+	// 	headers,
+	// 	body: JSON.stringify(products),
+	// }
+
+	// } catch (error) {
+	// 	console.log('error', error)
+
+	// 	return {
+	// 		statusCode: 400,
+	// 		headers,
+	// 		body: JSON.stringify(error),
+	// 	}
+	// }
+
+
 }
