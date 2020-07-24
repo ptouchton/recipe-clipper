a70c2c204664d2d379a62385443a9d34707c427e
Paul Touchton <Paul.Touchton@nike.com>
2020-07-23 17:21:33 -0700

    finally got the jwt authorization working

--- functions/products.js
+++ functions/products.js
@@ -2,6 +2,7 @@ import { ProductService } from '../lib/product.service.js'
 import { client, headers } from '../lib/config.js'
 import jwt from 'jsonwebtoken';
 import jwks from 'jwks-rsa';
+const { promisify } = require("util");
 
 const service = new ProductService({ client })
 
@@ -22,66 +23,63 @@ exports.handler = async (event, context) => {
 		return { statusCode: 405, headers, body: 'Method Not Allowed' }
 	};
 
-	// Set up Auth0 configuration
-	const authConfig = {
-		domain: 'dev-1ri5oc4g.us.auth0.com',
-		audience: 'http://localhost:9000/.netlify/function',
-	};
+	try {
+
+		// Set up Auth0 configuration
+		const authConfig = {
+			domain: 'dev-1ri5oc4g.us.auth0.com',
+			audience: 'http://localhost:9000/.netlify/functions',
+		};
 
 
-	const client = jwks({
-		strictSsl: true, // Default value
-		jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
-		requestHeaders: {}, // Optional
-		requestAgentOptions: {}, // Optional
-		timeout: 3000, // Defaults to 30s
-	});
-
-	function getKey(header, callback) {
-		client.getSigningKey(header.kid, function (err, key) {
-			let signingKey = key.publicKey || key.rsaPublicKey;
-			callback(null, signingKey);
+		const myClient = jwks({
+			strictSsl: true, // Default value
+			jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
+			requestHeaders: {}, // Optional
+			requestAgentOptions: {}, // Optional
+			timeout: 3000, // Defaults to 30s
 		});
-	}
 
-	const authHeader = event.headers.authorization;
-	console.log(`jwt: ${event.headers.authorization}`);
+		const authHeader = event.headers.authorization;
 
-	const parts = authHeader.split(" ");
-	if (parts.length !== 2 || parts[0] !== "Bearer") {
-		// No access token was passed
-		return null;
-	}
-	const accessToken = parts[1];
-
-	console.log(`token: ${accessToken}`);
-
-	// This could fail.  If it does handle as 401 as the token is invalid.
-	var decodedToken = jwt.decode(accessToken, { complete: true });
-	console.log(`decoded token: ${decodedToken.header.kid}`);
-
-	const header = decodedToken.header;
-	const decoded = jwt.verify(accessToken, getKey, { audience: authConfig.audience }, function (err, decoded) {
-
-		if (err) {
-			console.log(`err: ${err}`);
-			
-			// return {
-			// 	statusCode: 401,
-			// 	headers,
-			// 	body: 'Unauthorized',
-			// }
+		const parts = authHeader.split(" ");
+		if (parts.length !== 2 || parts[0] !== "Bearer") {
+			// No access token was passed
+			return null;
 		}
-		console.log(`verified: ${decoded}`);
-	});
+		const accessToken = parts[1];
 
+		// This could fail.  If it does handle as 401 as the token is invalid.
+		var decodedToken = jwt.decode(accessToken, { complete: true });
+
+		// promisify the function:
+		const getSigningKey = promisify(myClient.getSigningKey);
+
+		// use it like any other promise:
+		const key = await getSigningKey(decodedToken.header.kid);
+
+		const decoded = jwt.verify(accessToken, key.publicKey, { audience: authConfig.audience });
+		
+		const products = await service.getProducts();
+
+		return {
+			statusCode: 200,
+			headers,
+			body: JSON.stringify(products),
+		}
+
+	} catch (error) {
+		console.log('error', error)
+
+		return {
+			statusCode: 400,
+			headers,
+			body: JSON.stringify(error),
+		}
+	}
 
 	// const products = await service.getProducts()
-	// return {
-	// 	statusCode: 200,
-	// 	headers,
-	// 	body: JSON.stringify(products),
-	// }
+
 
 	// } catch (error) {
 	// 	console.log('error', error)
