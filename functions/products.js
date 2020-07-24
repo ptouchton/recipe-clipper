b6ca1ca95f6d51cc5b3a173486594655ab1b4481
Paul Touchton <Paul.Touchton@nike.com>
2020-07-23 20:04:02 -0700

    jwt verify refactor

--- functions/products.js
+++ functions/products.js
@@ -1,15 +1,12 @@
-import { ProductService } from '../lib/product.service.js'
-import { client, headers } from '../lib/config.js'
-import jwt from 'jsonwebtoken';
-import jwks from 'jwks-rsa';
-const { promisify } = require("util");
+import { ProductService } from '../lib/product.service.js';
+import { AuthCheck } from '../lib/auth.check';
+import { client, headers } from '../lib/config.js';
 
-const service = new ProductService({ client })
-
+const service = new ProductService({ client });
+const auth = new AuthCheck();
 
 exports.handler = async (event, context) => {
 	console.log('Function `products` invoked')
-	console.log(context);
 
 	if (event.httpMethod === 'OPTIONS') {
 		return {
@@ -24,42 +21,9 @@ exports.handler = async (event, context) => {
 	};
 
 	try {
-
-		// Set up Auth0 configuration
-		const authConfig = {
-			domain: 'dev-1ri5oc4g.us.auth0.com',
-			audience: 'http://localhost:9000/.netlify/functions',
-		};
-
-
-		const myClient = jwks({
-			strictSsl: true, // Default value
-			jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`,
-			requestHeaders: {}, // Optional
-			requestAgentOptions: {}, // Optional
-			timeout: 3000, // Defaults to 30s
-		});
-
-		const authHeader = event.headers.authorization;
-
-		const parts = authHeader.split(" ");
-		if (parts.length !== 2 || parts[0] !== "Bearer") {
-			// No access token was passed
-			return null;
-		}
-		const accessToken = parts[1];
-
-		// This could fail.  If it does handle as 401 as the token is invalid.
-		var decodedToken = jwt.decode(accessToken, { complete: true });
-
-		// promisify the function:
-		const getSigningKey = promisify(myClient.getSigningKey);
-
-		// use it like any other promise:
-		const key = await getSigningKey(decodedToken.header.kid);
-
-		const decoded = jwt.verify(accessToken, key.publicKey, { audience: authConfig.audience });
 		
+		const token = await auth.verify(event);
+
 		const products = await service.getProducts();
 
 		return {
@@ -77,19 +41,4 @@ exports.handler = async (event, context) => {
 			body: JSON.stringify(error),
 		}
 	}
-
-	// const products = await service.getProducts()
-
-
-	// } catch (error) {
-	// 	console.log('error', error)
-
-	// 	return {
-	// 		statusCode: 400,
-	// 		headers,
-	// 		body: JSON.stringify(error),
-	// 	}
-	// }
-
-
 }
